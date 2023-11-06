4ea93fed69a39258c1ac8d0959836baea42ad303
KiruthikaKanagu <kkiruthika810@gmail.com>
2023-11-06 16:55:57 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const ethereumBtn = document.getElementById("btn-custom-nav");
+const dropdownBtn = document.getElementById("dropdown");
+const send = document.getElementById("js-send")
+console.log(send);
+ethereumBtn.addEventListener("click", () => {
+
+    dropdownBtn.classList.toggle("invisible");
+
+
+});
